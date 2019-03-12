30d07f20388ecf04dad9d920f6fe70536396b99f
T4T4el7 <ochoaarmando13@gmail.com>
2019-03-11 18:25:09 -0700

    attempted some lst minute function revison and modal but didnt exactly work.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,6 +6,20 @@ module.exports = function(app) {
     });
     app.post("/api/friends", function(req, res) {
         friendsArray.push(req.body);
-        //res.json(true);
+       
     });
+    // app.post("/api/friends", function(req, res) {
+    //     var totDiff;
+    //     var diffArry = [];
+    //     var newFriend = req.body;
+    //     for (var i = 0; i < friendsArray.length; i++) {
+    //         totDiff = 0;
+    //         for (var j = 0; j < newFriend.scores.length; j++) {
+    //             totDiff += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+    //         } //for j
+    //         diffArry.push(totDiff);
+    //     }
+    //     var bestFriend = diffArry.indexOf(Math.min(...diffArry));
+       
+    // });
 }
